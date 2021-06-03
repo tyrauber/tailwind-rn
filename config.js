24e71da98cb9edd7abc163c9362166342c620a62
Ty Rauber <tyrauber@mac.com>
2021-06-03 12:18:54 -0400

    Fix formatting

--- config.js
+++ config.js
@@ -2,25 +2,25 @@ const tailwind = require('tailwindcss');
 const path = require('path');
 
 const config = {
-	paths: (options = {config: 'tailwind.config.js', styles: 'styles.json'}) => {
-		try {
-			const configPath = config.getFilePath(`./postcss.config.js`);
-			options = Object.assign(options, configPath.plugins.tailwindcss);
-		} catch (_) {}
+  paths: (options = {config: 'tailwind.config.js', styles: 'styles.json'}) => {
+    try {
+      const configPath = config.getFilePath(`./postcss.config.js`);
+      options = Object.assign(options, configPath.plugins.tailwindcss);
+    } catch (_) {}
 
-		return options;
+    return options;
 	},
-	getFilePath: name => {
+  getFilePath: name => {
     const filePath = path.join(__dirname,config.paths()[name]);
     console.log('getFilePath', filePath);
     return filePath;
-	},
-	getTailwind: () => {
-		try {
-			return tailwind(config.getFilePath('config'));
-		} catch (_) {
-			return tailwind;
-		}
+  },
+  getTailwind: () => {
+    try {
+      return tailwind(config.getFilePath('config'));
+    } catch (_) {
+      return tailwind;
+    }
 	}
 };
 
